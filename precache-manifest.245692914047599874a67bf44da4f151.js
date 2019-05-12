b24a7830d8797cb1ea68101c7d9daab30a1d4a30
Yuito <yuitosato.w@gmail.com>
2019-05-12 16:45:26 +0900

    Updates

---
+++ precache-manifest.245692914047599874a67bf44da4f151.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20a295d4addcacccaf25918e4b7f917a",
+    "url": "/sidefile-selenide/index.html"
+  },
+  {
+    "revision": "1caf2e2a7e5499ae0ee1",
+    "url": "/sidefile-selenide/static/css/main.ddd17464.chunk.css"
+  },
+  {
+    "revision": "c914f5745e674268b802",
+    "url": "/sidefile-selenide/static/js/2.6bac8f0f.chunk.js"
+  },
+  {
+    "revision": "1caf2e2a7e5499ae0ee1",
+    "url": "/sidefile-selenide/static/js/main.9b302871.chunk.js"
+  },
+  {
+    "revision": "441a8c8fef4d545ee6db",
+    "url": "/sidefile-selenide/static/js/runtime~main.e8240e62.js"
+  }
+]);
